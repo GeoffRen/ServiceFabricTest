67c612718b5e4fcc7cd08ee50faf23991b279e2b
GeoffRen <geren@microsoft.com>
2020-09-16 14:00:56 -0600

    Added stateful service

--- build.cmd
+++ build.cmd
@@ -4,3 +4,9 @@ dotnet build %~dp0\..\ServiceFabricTest\src\ServiceFabricTest\UnattendedService\
 for %%F in ("%~dp0\..\ServiceFabricTest\src\ServiceFabricTest\UnattendedService\UnattendedService.csproj") do cd %%~dpF
 dotnet publish -o %~dp0\..\ServiceFabricTest\ServiceFabricTest\UnattendedServicePkg\Code
 cd %~dp0\..
+
+          
+dotnet restore %~dp0\..\ServiceFabricTest\src\ServiceFabricTest\Cache\Cache.csproj -s https://api.nuget.org/v3/index.json           
+dotnet build %~dp0\..\ServiceFabricTest\src\ServiceFabricTest\Cache\Cache.csproj -v normal          
+for %%F in ("ServiceFabricTest\src\ServiceFabricTest\Cache\Cache.csproj") do cd %%~dpF          
+dotnet publish -o %~dp0\..\ServiceFabricTest\ServiceFabricTest\CachePkg\Code
